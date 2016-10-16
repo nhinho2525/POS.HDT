ef2b262132e9fbdd028b144bde52cf385d3ef940
nhinho2525 <nhinho2525@gmail.com>
2016-10-16 23:38:28 +0700

    Add to domain: Ingredient, Input

---
+++ POS.HDT.Common.Data.Domain/Ingredients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.HDT.Common.Data.Domain
+{
+    public class Ingredients
+    {
+        public string IngredientId { get; set; }
+        public string IngredientName { get; set; }
+        public string IngredientMeasure { get; set; }
+        public string IngredientExchange { get; set; }
+        public string IngredientUnit { get; set; }
+    }
+}
---
+++ POS.HDT.Common.Data.Domain/Inputs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.HDT.Common.Data.Domain
+{
+    public class Inputs
+    {
+        public string InputId { get; set; }
+        public string Sophieu { get; set; }
+        public string SophieuDate { get; set; }
+        public string RecieptId { get; set; }
+        public string RecieptDate { get; set; }
+        public string SupplierId { get; set; }
+        public string StoreId { get; set; }
+        public string Paid { get; set; }
+        public string InputDiscount { get; set; }
+        public string InputNote { get; set; }
+        public string UserId { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
